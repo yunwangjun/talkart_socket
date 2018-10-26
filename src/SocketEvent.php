6b563173b9efbd7a6184e96941b5307684f15077
贠王军 <yunwangjun@talkart.cc>
2018-10-26 17:08:25 +0800

    add src/SocketEvent

---
+++ src/SocketEvent.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * This file is part of talkart_socket.
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the MIT-LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author    贠王军<yunwangjun@appleoct.com>
+ * @copyright 贠王军<yunwangjun@appleoct.com>
+ * @link      http://www.yunwangjun.cn/
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+
+namespace TalkArtSocket;
+/**
+ * Autoload.
+ */
+class SocketEvent{
+
+    public static function publush(){
+
+        echo date();
+    }
+
+    public static function notice($subject, $event, $data, $exclude){
+
+        foreach ($subject_connnection_map[$subject] as $connection) {
+            if ($exclude == $connection) {
+                continue;
+            }
+            $connection->send(json_encode(array(
+                'cmd'   => 'publish',
+                'event' => $event,
+                'data'  => $data
+            )));
+        }
+    }
+}
